ab5a884a509aa1385187227a3b6bca56be0364d8
Kael <i@kael.me>
2016-06-24 10:48:33 +0800

    main

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 'use strict'
 
+module.exports = walker
+
+const Walker = require('./walker')
+const { parseDependenciesFromAST } = require('./dependency')
+const { astFromSource } = require('./utils')
+
+function walker (options) {
+  return new Walker(options)
+}
+
+
+walker.parseDependenciesFromAST = parseDependenciesFromAST
+walker.astFromSource = astFromSource
